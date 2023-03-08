f096f2f6ca1a35a026e7287f4300a70a618fb08d
peter <secret_wings@naver.com>
2023-03-08 16:32:06 +0900

    feat: 유저 정보 조회 api 구현

---
+++ src/pages/api/users/[id].ts
@@ -0,0 +1,42 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import assert from "assert";
+import { User } from "@/api/server/generated";
+import { NetworkResult } from "@/interface/network";
+import client from "@/libs/client";
+import withHandler from "@/libs/server/withHandler";
+
+interface CheckUserResponse {
+  user: User;
+}
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<NetworkResult<CheckUserResponse>>
+) {
+  const userId = req.query.id;
+  try {
+    const user = await client.user.findUnique({
+      where: { id: userId as string },
+      include: {
+        union: true,
+      },
+    });
+    assert(user !== null, "user is null");
+
+    res.status(200).json({
+      status: "Success",
+      data: {
+        user,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      status: "Failed",
+      message: "DB Error",
+    });
+  }
+}
+
+export default withHandler({ methods: ["GET"], handler, isPrivate: true });
