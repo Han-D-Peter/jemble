5670a0048d7a224bda0c8f4ac7d1d10e60307a29
peter <secret_wings@naver.com>
2023-03-08 19:09:04 +0900

    feat: 친구 요청 api 구현

--- src/pages/api/friends/index.ts
+++ src/pages/api/friends/index.ts
@@ -2,27 +2,26 @@
 import { PrismaClient } from "@/api/server/generated";
 import { Union } from "@/interface/model/union";
 import { User } from "@/interface/model/user";
-import { NetworkResult } from "@/interface/network";
+import { NetworkResult, RequestFriendStatus } from "@/interface/network";
 import client from "@/libs/client";
 import withHandler from "@/libs/server/withHandler";
 import assert from "assert";
 import type { NextApiRequest, NextApiResponse } from "next";
-import { getServerSession } from "next-auth";
+import { getServerSession, Session } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]";
 
 interface CheckFriendsResponse {
   friends: User[];
 }
 
-async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<NetworkResult<CheckFriendsResponse>>
+interface CreateFriendResponse {}
+
+async function getFriends(
+  session: Session,
+  res: NextApiResponse<NetworkResult<CheckFriendsResponse>>,
+  req: NextApiRequest
 ) {
   const searchKeyWord = req.query.search;
-  const session = await getServerSession(req, res, authOptions);
-
-  assert(session !== null, "session is null");
-
   try {
     const friends = await client.user.findUnique({
       where: {
@@ -68,4 +67,78 @@ async function handler(
   }
 }
 
-export default withHandler({ methods: ["GET"], handler, isPrivate: true });
+async function requestFriends(
+  session: Session,
+  res: NextApiResponse<
+    NetworkResult<CheckFriendsResponse | CreateFriendResponse>
+  >,
+  req: NextApiRequest
+) {
+  const targetFriendId = req.body.userId;
+
+  assert(targetFriendId !== null, "id is null");
+
+  try {
+    const existRequestArr = await client.requestFriendTransaction.findMany({
+      where: {
+        sender: session.user.id,
+        receiver: targetFriendId,
+      },
+    });
+    if (existRequestArr.length === 0) {
+      await client.requestFriendTransaction.create<{
+        data: {
+          sender: string;
+          receiver: string;
+          status: RequestFriendStatus;
+        };
+      }>({
+        data: {
+          sender: session.user.id,
+          receiver: targetFriendId,
+          status: "Pending",
+        },
+      });
+
+      res.status(200).json({
+        status: "Success",
+        message: "요청 완료",
+      });
+      return;
+    }
+    res.status(400).json({
+      status: "Failed",
+      message: "이미 요청한 친구입니다.",
+    });
+    return;
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      status: "Failed",
+      message: "DB Error",
+    });
+  }
+}
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<
+    NetworkResult<CheckFriendsResponse | CreateFriendResponse>
+  >
+) {
+  const session = await getServerSession(req, res, authOptions);
+
+  assert(session !== null, "session is null");
+  if (req.method === "GET") {
+    getFriends(session, res, req);
+  }
+
+  if (req.method === "POST") {
+    requestFriends(session, res, req);
+  }
+}
+
+export default withHandler({
+  methods: ["GET", "POST"],
+  handler,
+  isPrivate: true,
+});
