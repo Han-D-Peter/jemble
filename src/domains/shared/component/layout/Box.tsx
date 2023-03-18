ea29ab9a2593a0260bbfd135c5ef685308d20e25
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2023-03-18 21:14:26 +0900

    feat: 공용 박스 컴포넌트

---
+++ src/domains/shared/component/layout/Box.tsx
@@ -0,0 +1,33 @@
+import { css } from "@emotion/react";
+import { ReactNode } from "react";
+
+interface BoxProps {
+  title?: string;
+  children?: ReactNode | null;
+}
+
+const boxStyle = css`
+  background: #fff;
+  width: 100%;
+  border-radius: 7px;
+  margin: 5px 0 5px 0;
+`;
+
+const innerBoxStyle = css`
+  padding: 24px;
+`;
+
+const titleStyle = css`
+  font-size: 22px;
+`;
+
+export default function Box({ title, children }: BoxProps) {
+  return (
+    <section css={boxStyle}>
+      <div css={innerBoxStyle}>
+        {title && <h1 css={titleStyle}>{title}</h1>}
+        {children}
+      </div>
+    </section>
+  );
+}
