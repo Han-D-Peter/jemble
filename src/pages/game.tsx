ccf486aa6ce6189a9609980b231bf1c476683adf
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2023-03-18 19:34:23 +0900

    feat: 인증 확인 hoc 페이지 프로텍트

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -1,9 +1,13 @@
 import DefaultLayout from "@/domains/shared/component/layout/DefaultLayout";
+import withAuthentication from "@/domains/shared/hoc/auth/withAuthentication";
+import { NextPage } from "next";
 
-export default function Game() {
+const Game: NextPage = () => {
   return (
     <DefaultLayout isLogined>
       <div>Game</div>
     </DefaultLayout>
   );
-}
+};
+
+export default withAuthentication(Game);
