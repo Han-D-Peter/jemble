2c9a262127833cd5570cf3feac5c043f87570183
peter <secret_wings@naver.com>
2023-03-18 15:28:34 +0900

    feat: 공통레이아웃 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,15 @@ import {
   useRequestFriendMutation,
 } from "@/domains/query-hook/queries/friends";
 import { useGetTransactions } from "@/domains/query-hook/queries/transactions";
+import DefaultLayout from "@/domains/shared/component/layout/DefaultLayout";
 
 export default function Home() {
   const { data } = useGetTransactions();
 
   console.log("data", data);
   return (
-    <>
+    <DefaultLayout isLogined>
       <div>hello</div>
-    </>
+    </DefaultLayout>
   );
 }
