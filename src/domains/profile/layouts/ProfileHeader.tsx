bc5ccf09b9dccd4f69d0ebe8a26e5b9ba0357a55
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2023-03-21 00:38:07 +0900

    fix: 소속 유니온 없을 때 헤더 분기처리

--- src/domains/profile/layouts/ProfileHeader.tsx
+++ src/domains/profile/layouts/ProfileHeader.tsx
@@ -6,7 +6,7 @@ interface ProfileHeaderProps {}
 export default function ProfileHeader({}: ProfileHeaderProps) {
   const { data, isError } = useGetMyUnion();
 
-  if (!data?.data || isError) return <div>Not Found</div>;
+  if (!data?.data || isError) return <div>소속된 유니온이 없습니다.</div>;
 
   return <UnionWithRank unionName={data.data.name} />;
 }
