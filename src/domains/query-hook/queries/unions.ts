85c7f03d671cbe8c0524d685b47bc1bd8a53aa77
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2023-03-21 00:48:30 +0900

    fix: 통신 에러 예외처리

--- src/domains/query-hook/queries/unions.ts
+++ src/domains/query-hook/queries/unions.ts
@@ -2,31 +2,47 @@ import { useMutation, useQuery } from "@tanstack/react-query";
 import UnionRepository, { CreateUnionPayload } from "../repository/unions";
 
 export const useGetMyUnion = () => {
-  return useQuery(["myUnion"], UnionRepository.getGetMyUnion);
+  return useQuery(["myUnion"], UnionRepository.getGetMyUnion, {
+    onError: error => console.error(error),
+  });
 };
 
 export const useGetUnions = () => {
-  return useQuery(["unions"], UnionRepository.getUnions);
+  return useQuery(["unions"], UnionRepository.getUnions, {
+    onError: error => console.error(error),
+  });
 };
 
 export const useGetUnion = (name: string) => {
-  return useQuery(["union", name], () => UnionRepository.getUnion(name));
+  return useQuery(["union", name], () => UnionRepository.getUnion(name), {
+    onError: error => console.error(error),
+  });
 };
 
 export const useGetUnionsRank = () => {
-  return useQuery(["unionsRank"], UnionRepository.getUnionsRank);
+  return useQuery(["unionsRank"], UnionRepository.getUnionsRank, {
+    onError: error => console.error(error),
+  });
 };
 
 export const useGetUnionRank = (name: string) => {
-  return useQuery(["unionRank", name], () =>
-    UnionRepository.getUnionRank(name)
+  return useQuery(
+    ["unionRank", name],
+    () => UnionRepository.getUnionRank(name),
+    {
+      onError: error => console.error(error),
+    }
   );
 };
 
 export const useCreateUnionMutation = () => {
-  return useMutation(["createUnion"], UnionRepository.createUnion);
+  return useMutation(["createUnion"], UnionRepository.createUnion, {
+    onError: error => console.error(error),
+  });
 };
 
 export const useJoinUnionMutation = () => {
-  return useMutation(["joinUnion"], UnionRepository.joinUnion);
+  return useMutation(["joinUnion"], UnionRepository.joinUnion, {
+    onError: error => console.error(error),
+  });
 };
