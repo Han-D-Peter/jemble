d021e8f502861d18c661a0aa1a2b38724192e417
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2023-03-20 18:01:32 +0900

    feat: 기능 훅 구현

---
+++ src/domains/shared/hooks/useModal.tsx
@@ -0,0 +1,35 @@
+import { useOverlay } from "@toss/use-overlay";
+import { ComponentProps, MouseEvent, TouchEvent, useRef } from "react";
+import SuspensableBox from "@/domains/shared/component/layout/SuspensableBox";
+import DefaultModal from "../component/layout/DefaultModal";
+import { useOnClickOutside } from "./useOnClickOutside";
+
+export default function useModal() {
+  const overlay = useOverlay();
+  const ref = useRef<HTMLDivElement>(null);
+
+  useOnClickOutside(ref, () => {
+    overlay.close();
+  });
+
+  const openModal = (props: ComponentProps<typeof SuspensableBox>) =>
+    new Promise<boolean>(resolve => {
+      overlay.open(({ isOpen, close }) => {
+        return (
+          <>
+            {isOpen ? (
+              <DefaultModal
+                onClick={(e: MouseEvent) => {
+                  e.target === ref.current ? close() : null;
+                  resolve(false);
+                }}
+              >
+                <SuspensableBox ref={ref} {...props} />
+              </DefaultModal>
+            ) : null}
+          </>
+        );
+      });
+    });
+  return { open: openModal, close: overlay.close };
+}
