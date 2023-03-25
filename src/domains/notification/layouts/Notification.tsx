cdf25d45ba42b77f7ce5297faf743a1c84b36b13
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2023-03-25 23:27:38 +0900

    feat: Noti 페이지 구현

---
+++ src/domains/notification/layouts/Notification.tsx
@@ -0,0 +1,26 @@
+import { useGetTransactions } from "@/domains/query-hook/queries/transactions";
+import Spinner from "@/domains/shared/component/Spinner";
+import { parseTransactions } from "@/domains/shared/utils/componentUtils";
+import { css } from "@emotion/react";
+import { useSession } from "next-auth/react";
+import { Suspense } from "react";
+
+interface NotificationBoxProps {}
+
+const notificationStyle = css``;
+
+export default function NotificationBox({}: NotificationBoxProps) {
+  const me = useSession();
+  const { data } = useGetTransactions();
+  return (
+    <div css={notificationStyle}>
+      {data?.data?.transactions.map(item => {
+        return (
+          <Suspense fallback={<Spinner size="sm" />}>
+            {parseTransactions(item, me.data?.user.id as string)}
+          </Suspense>
+        );
+      })}
+    </div>
+  );
+}
